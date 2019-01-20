5c4c8ffdd1a6087d80c8e35b72376d7464998a3e
ringaile <ringaile@gmail.com>
2019-01-20 18:55:52 +0100

    added profiles

--- src/main/java/com/ringaile/springbootexample/mail/MailConfig.java
+++ src/main/java/com/ringaile/springbootexample/mail/MailConfig.java
@@ -3,15 +3,18 @@ package com.ringaile.springbootexample.mail;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MailConfig {
 
     @Bean
+    @Profile("dev")
     public MailSender mockMailSender(){
         return new MockMailSender();
     }
     @Bean
+    @Profile("!dev")
     public MailSender smtpMailSender(){
         return new SmtpMailSender();
     }
